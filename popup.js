75afd1e21c2cf6f3e97a8325299097b5031552d0
sabrinaelee <67843130+sabrinaelee@users.noreply.github.com>
2021-01-16 12:51:18 -0500

    refactoring

--- popup.js
+++ popup.js
@@ -1,22 +1,11 @@
+document.querySelector(".happy").addEventListener("click", function () {
+   // alert("congrats u r happy");
+  });
 
+  document.querySelector(".neutral").addEventListener("click", function () {
+    //alert("congrats u r neutral");
+  });
 
- document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
-    }, false);
-  }, false);
+  document.querySelector(".sad").addEventListener("click", function () {
+    //alert("congrats u r sad");
+  });
