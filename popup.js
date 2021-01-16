b228b38ed1fd2cf77594131d50385eb859eadf9b
thebilogis <75547025+thebilogis@users.noreply.github.com>
2021-01-16 13:21:05 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,22 +1,26 @@
 
 
- document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
-    }, false);
-  }, false);
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+document.querySelector(".date");
+let curDate = new Date();
+let curMon = monthNames[curDate.getMonth()];
+let curDat = curDate.getDate();
+let curMin = curDate.getMinutes();
+let curSec = curDate.getSeconds();
+const message = document.createElement("div");
+
+message.innerHTML = `Date: ${curMon} ${curDat}`;
+document.querySelector(".body__popup").append(message);
