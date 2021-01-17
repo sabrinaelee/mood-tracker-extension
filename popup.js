77f0715127cc67b7ced3d300adebc2d1b115b618
Joshua <ngojosh.j@gmail.com>
2021-01-17 08:44:51 -0500

    Refactored

--- popup.js
+++ popup.js
@@ -12,6 +12,7 @@ const monthNames = [
   "November",
   "December",
 ];
+
 const head = document.querySelector(".head");
 const happy = document.getElementById("happybutton");
 const neutral = document.getElementById("neutralbutton");
@@ -27,13 +28,27 @@ const numDaysInMonth = lastDayInMonth.getDate();
 let curDat = curDate.getDate();
 let curMin = curDate.getMinutes();
 let curSec = curDate.getSeconds();
+const lastDay = [
+  31,
+  new Date(curYear, 2, 0).getDate(),
+  31,
+  30,
+  31,
+  30,
+  31,
+  31,
+  30,
+  31,
+  30,
+  31,
+];
 const message = document.createElement("h3");
 const message2 = document.createElement("h1");
+let currentSlide = 0;
 let moodObj = {};
 
 for (let i = 1; i <= numDaysInMonth; i++) {
   moodObj[i] = 0;
-  console.log("hi");
 }
 
 message.innerHTML = `${curMon} ${curDat}, ${curYear}`;
@@ -45,12 +60,56 @@ document.querySelector(".save")?.append(message2);
 const arrDate = Array.from({ length: numDaysInMonth }, () => 0);
 const buttonContainer = document.querySelector(".buttons");
 
-const daysHTML = document?.querySelector(".days");
+const daysHTML = document?.querySelectorAll(".days");
 let messageDays = "";
-for (let i = 1; i <= numDaysInMonth; i++) {
-  messageDays += `<div class="calendar btn btn--border" id="btn__${i}">${i}</div>`;
-  if (daysHTML) daysHTML.innerHTML = messageDays;
-}
+daysHTML.forEach(function (el, i) {
+  for (let j = 1; j <= lastDay[i]; j++) {
+    daysHTML[i].insertAdjacentHTML(
+      "beforeend",
+      `<div class="calendar btn btn--border ${
+        i == curDate.getMonth() ? "" : "nohover"
+      }" id="btn__${j}">${j}</div>`
+    );
+  }
+});
+
+const scrollRight = function (curSlide) {
+  daysHTML.forEach((s, i) => {
+    if (i == curSlide) {
+      daysHTML[i].classList.add("hidden");
+    } else if (i == curSlide + 1) {
+      daysHTML[i].classList.remove("hidden");
+      message2.innerHTML = `${monthNames[i]}`;
+    }
+  });
+  currentSlide++;
+};
+const scrollLeft = function (curSlide) {
+  daysHTML.forEach((s, i) => {
+    if (i == curSlide) {
+      daysHTML[i].classList.add("hidden");
+    } else if (i == curSlide - 1) {
+      daysHTML[i].classList.remove("hidden");
+      message2.innerHTML = `${monthNames[i]}`;
+    }
+  });
+  currentSlide--;
+};
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight" && currentSlide != 11) {
+    scrollRight(currentSlide);
+  }
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft" && currentSlide != 0) {
+    scrollLeft(currentSlide);
+  }
+});
+// for (let i = 1; i <= lastDay[i - 1]; i++) {
+//   messageDays += `<div class="calendar btn btn--border" id="btn__${i}">${i}</div>`;
+//   if (daysHTML) daysHTML.innerHTML = messageDays;
+// }
 
 const updateButton = function (moods, day) {
   const divLocation = document.querySelector(`#btn__${day}`);
@@ -99,51 +158,18 @@ const updateColors = function (moods) {
 window.addEventListener("DOMContentLoaded", function () {
   chrome.storage.sync.get(["moods"], function (result) {
     try {
-      console.log(result.moods);
+      // console.log(result.moods);
       if (result.moods) {
         moodObj = result.moods;
-        console.log("my value was ", result.moods[curDat]);
+        // console.log("my value was ", result.moods[curDat]);
         updateButton(result.moods, curDat);
         updateColors(result.moods);
-        console.log(document.querySelectorAll("btn--border"));
+        // console.log(document.querySelectorAll("btn--border"));
       }
     } catch {}
   });
 });
 
-/*
-const btn_1 = document.getElementById("btn__1");
-const btn_2 = document.getElementById("btn__2");
-const btn_3 = document.getElementById("btn__3");
-const btn_4 = document.getElementById("btn__4");
-const btn_5 = document.getElementById("btn__5");
-const btn_6 = document.getElementById("btn__6");
-const btn_7 = document.getElementById("btn__7");
-const btn_8 = document.getElementById("btn__8");
-const btn_9 = document.getElementById("btn__9");
-const btn_10 = document.getElementById("btn__10");
-const btn_11 = document.getElementById("btn__11");
-const btn_12 = document.getElementById("btn__12");
-const btn_13 = document.getElementById("btn__13");
-const btn_14 = document.getElementById("btn__14");
-const btn_15 = document.getElementById("btn__15");
-const btn_16 = document.getElementById("btn__16");
-const btn_17 = document.getElementById("btn__17");
-const btn_18 = document.getElementById("btn__18");
-const btn_19 = document.getElementById("btn__19");
-const btn_20 = document.getElementById("btn__20");
-const btn_21 = document.getElementById("btn__21");
-const btn_22 = document.getElementById("btn__22");
-const btn_23 = document.getElementById("btn__23");
-const btn_24 = document.getElementById("btn__24");
-const btn_25 = document.getElementById("btn__25");
-const btn_26 = document.getElementById("btn__26");
-const btn_27 = document.getElementById("btn__27");
-const btn_28 = document.getElementById("btn__28");
-const btn_29 = document.getElementById("btn__29");
-const btn_30 = document.getElementById("btn__30");
-const btn_31 = document.getElementById("btn__31");
-*/
 for (let i = 1; i <= numDaysInMonth; i++) {
   document.getElementById(`btn__${i}`)?.addEventListener("click", function () {
     chrome.storage.sync.get(["moods"], function (result) {
